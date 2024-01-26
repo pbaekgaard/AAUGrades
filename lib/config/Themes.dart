e6b497d0a756f939f7dafebc5c0bf5a288b715e4
pbaekgaard <pbakga21@student.aau.dk>
2024-01-26 22:45:37 +0100

    created navigation bar

---
+++ lib/config/Themes.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+ThemeData light = ThemeData.from(
+  colorScheme: const ColorScheme.light(
+      brightness: Brightness.light,
+      background: Color(0xFFFFFFFF),
+      onBackground: Color(0xFF4a4a4a),
+      secondary: Color(0xFF00FFD1),
+      onSecondary: Color(0xFF7e7e7e),
+      primary: Color(0xFF28EECA),
+      secondaryContainer: Color(0xFFFFFFFF),
+      tertiary: Color.fromARGB(255, 50, 197, 55),
+      primaryContainer: Colors.white),
+);
+
+ThemeData dark = ThemeData.from(
+    colorScheme: const ColorScheme.dark(
+  brightness: Brightness.dark,
+  background: Color(0xFF363636),
+  onBackground: Color(0xFFFFFFFF),
+  secondary: Color(0xFF00FFD1),
+  onSecondary: Color(0xFF7e7e7e),
+  secondaryContainer: Color(0xFF444444),
+  primary: Color(0xFF28EECA),
+  primaryContainer: Color(0xFF3f3f3f),
+  tertiary: Color.fromARGB(255, 50, 197, 55),
+));
+
+class ThemeService with ChangeNotifier {
+  ThemeMode? _themeMode;
+  ThemeMode defaultThemeMode = ThemeMode.system;
+
+  Future init() async {
+    await _getThemeModeFromSharedPrefs();
+  }
+
+  ThemeService() {
+    _getThemeModeFromSharedPrefs();
+  }
+
+  ThemeMode get themeMode {
+    return _themeMode ?? ThemeMode.light;
+  }
+
+  set themeMode(ThemeMode themeMode) {
+    _themeMode = themeMode;
+    _saveThemeModeInSharedPrefs(themeMode);
+    notifyListeners();
+  }
+
+  Future _getThemeModeFromSharedPrefs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? themeModeFromPrefs = prefs.getString('themeMode');
+    _themeMode = ThemeMode.values.firstWhere(
+        (element) => themeModeFromPrefs == element.toString(),
+        orElse: () => defaultThemeMode);
+    notifyListeners();
+  }
+
+  _saveThemeModeInSharedPrefs(ThemeMode themeMode) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('themeMode', themeMode.toString());
+  }
+}
---
+++ lib/pages/AllGradesPage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AllGradesPage extends StatefulWidget {
+  const AllGradesPage({Key? key}) : super(key: key);
+
+  @override
+  _AllGradesPageState createState() => _AllGradesPageState();
+}
+
+class _AllGradesPageState extends State<AllGradesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("AllGradesPage"),
+      ),
+    );
+  }
+}
